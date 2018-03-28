e822f29c2be87514ddf716a605b25f66823a640f
Stavros Trakoshis <stauros.trak@gmail.com>
2018-03-28 13:12:01 +0300

    NaNs
    added identification of NaNs

--- Fractal_loop.m
+++ Fractal_loop.m
@@ -1,4 +1,4 @@
-%ombining parellate.m and bfn_mfin_ml.m in MR subjets
+%Combining parellate.m and bfn_mfin_ml.m in MR subjets
 %Assuming 3dresample with rest as -master was run and result was saved in a
 %HURSTS dir, in subjets repo as {'subjetsID_MMP_in_MNI_symmetrial_1.nii.gz'}
 %and its the only one in the dir
@@ -26,18 +26,47 @@ for i=1:length(d)
     disp(subpath);
     
     %parcellate args
-    atlasfile = fullfile(subpath,'preproc_2','HURSTS','Erest_TEMP.nii.gz');
+    atlasfile = fullfile(subpath,'HURSTS','Erest_TEMP.nii.gz');
     datafile = fullfile(subpath, 'preproc_2', 'Erest_pp.nii.gz');
     fname2save = strcat(subname,'_MMP_in_MNI_symmetrical_1.csv');
+    
+    
     %calls parcellate.m
     result = parcellate(atlasfile,datafile,fname2save,MEANCENTER);
-   
+    
    
    %import csv make it an array
     table = readtable(fname2save);
     disp(size(table));
     ArrayTable = table2array(table);
+
+    % Identify if there are any regions that the atlas is not covering  
    
+    colswnan = find(sum(isnan(ArrayTable),1)~=0);
+
+   
+    %feed array into bfn_mfin_ml.m
+    [H, nfcor, fcor] = bfn_mfin_ml(ArrayTable, 'filter', 'Haar', 'lb', lb, 'ub', ub);
+
+    % there are any columns with NAN replace them in ArrayTable with NAN
+	if !isempty(colswnan)
+		H(colswnan) = NaN;
+	end   
+
+    %write everything into csv for each subject
+    H_name = strcat(subname, '_H.csv');
+    fcor_name = strcat(subname, '_fcor.csv');
+    nfcor_name = strcat(subname, '_nfcor.csv');
+    
+   
+    csvwrite(H_name, H);
+    csvwrite(fcor_name, fcor);
+    csvwrite(nfcor_name, nfcor);
+    
+   
+    clear H fcor nfcor datafile atlasfile fname2save H_name fcor_name nfcor_name att 
+   
+end
     %feed array into bfn_mfin_ml.m
     [H, nfcor, fcor, att] = bfn_mfin_ml(ArrayTable, lb, ub, 'Haar');
    
